3f645cbf3d39b6886320016da3f08b68ab5447ac
Richard Lee <141524775+rikharthulee@users.noreply.github.com>
2025-08-02 14:21:18 +0100

    Create field constructor

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-const prompt = require('prompt-sync')({sigint: true});
+//importing prompt-sync for user input
+const prompt = require("prompt-sync")({ sigint: true });
 
-const hat = '^';
-const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
+// Constants for the game
+const hat = "^";
+const hole = "O";
+const fieldCharacter = "░";
+const pathCharacter = "*";
+
+// Class representing the game field
+class Field {
+  constructor(field) {
+    this.field = field;
+  }
+}
+
+const myField = new Field([
+  ["*", "░", "O"],
+  ["░", "O", "░"],
+  ["░", "^", "░"],
+]);
+
+myField.print();
